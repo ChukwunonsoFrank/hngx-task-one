f0ddae78a04a768e728c9284c1015d5b4911dd48
Chukwunonso Frank <franknonso114@gmail.com>
2023-09-08 09:48:29 +0100

    chore: clean up code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,23 +6,9 @@ export default {
     const dayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     function getUTCTimeWithAccuracy() {
-      // const currentMins = date.getUTCMinutes()
-      // const currentSecs = date.getUTCSeconds()
-
-      // const offset = (Math.random() - 0.5) * 4
-      // const offsetSecs = offset * 60
-
-      // const adjustedSecs = currentSecs + offsetSecs
-      // const adjustedMins = currentMins + Math.floor(adjustedSecs / 60)
-
-      // const finalMins = adjustedMins % 60
-
-      const adjustedDate = new Date()
-      // adjustedDate.setUTCHours(date.getUTCHours(), finalMins, Math.floor(adjustedSecs % 60))
-
-      const slice_start = adjustedDate.toISOString().indexOf('.')
-      const dateISOString = adjustedDate.toISOString().substring(0, slice_start).concat('Z')
-
+      const date = new Date()
+      const slice_start = date.toISOString().indexOf('.')
+      const dateISOString = date.toISOString().substring(0, slice_start).concat('Z')
       return dateISOString
     }
 
